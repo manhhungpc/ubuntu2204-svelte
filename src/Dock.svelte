93e92571ebb0b0988e62d477cd493aa9382b6af0
manhhungpc <phamhung2002@gmail.com>
2023-06-22 12:35:36 +0700

    feat: drag app dock demo

--- src/Dock.svelte
+++ src/Dock.svelte
@@ -1,5 +1,27 @@
 <script lang="ts">
-    const apps = [
+    import { flip } from "svelte/animate";
+    import { quintOut } from "svelte/easing";
+    import { crossfade } from "svelte/transition";
+    let hovering = 0;
+
+    const [send, receive] = crossfade({
+        duration: (d) => Math.sqrt(d * 200),
+
+        fallback(node, params) {
+            const style = getComputedStyle(node);
+            const transform = style.transform === "none" ? "" : style.transform;
+
+            return {
+                duration: 600,
+                easing: quintOut,
+                css: (t) => `
+					transform: ${transform} scale(${t});
+					opacity: ${t}
+				`,
+            };
+        },
+    });
+    let apps = [
         {
             id: 0,
             name: "gg-chrome",
@@ -26,18 +48,73 @@
             imgSrc: "/img/apps/system-settings.png",
         },
     ];
+
+    const dragStart = (event: DragEvent, target: any) => {
+        const element = event.target as HTMLElement;
+        const childImg = element.children[0];
+        childImg.style.backgroundColor = "transparent";
+        // childImg.style.opacity = "0.2";
+        // element.style.backgroundColor = "transparent";
+        // element.style.pointerEvents = "none";
+        // element.style.position = "relative";
+        // element.style.zIndex = "1";
+        // element.style.transform = "translate(0, 0)";
+        // element.style.pointerEvents = "none";
+
+        event.dataTransfer.effectAllowed = "move";
+        event.dataTransfer.dropEffect = "move";
+        event.dataTransfer.setData("text/plain", target);
+    };
+
+    const dragging = (event: DragEvent) => {
+        const element = event.target as HTMLElement;
+        const childImg = element.children[0];
+        childImg.style.backgroundColor = "transparent";
+        childImg.style.opacity = "0.2";
+        element.style.backgroundColor = "transparent";
+    };
+
+    const drop = (event: DragEvent, target: any) => {
+        const element = event.target as HTMLElement;
+        // element.style.border = "2px solid blue";
+        element.style.backgroundColor = "transparent";
+
+        event.dataTransfer.dropEffect = "move";
+        const start = parseInt(event.dataTransfer.getData("text/plain"));
+        const newTracklist = apps;
+
+        if (start < target) {
+            newTracklist.splice(target + 1, 0, newTracklist[start]);
+            newTracklist.splice(start, 1);
+        } else {
+            newTracklist.splice(target, 0, newTracklist[start]);
+            newTracklist.splice(start + 1, 1);
+        }
+        apps = newTracklist;
+        hovering = null;
+    };
 </script>
 
 <div class="wrap-dock">
     <div class="grid-apps">
-        {#each apps as app, i}
-            <button class="btn-app">
+        {#each apps as app, i (app.id)}
+            <button
+                class="btn-app"
+                in:receive={{ key: app.id }}
+                out:send={{ key: app.id }}
+                animate:flip
+                draggable="true"
+                on:dragstart={(event) => dragStart(event, i)}
+                on:drag={(event) => dragging(event)}
+                on:drop|preventDefault={(event) => drop(event, i)}
+                ondragover="return false"
+                on:dragenter={() => (hovering = i)}
+                class:is-active={hovering === i}
+            >
                 <img src={app.imgSrc} alt={app.name} class="app-icon" />
             </button>
-            {#if i === 3}
-                <hr class="w-12 my-2" style="border-color: var(--warm-grey); border-width: 1px 0 0 0;" />
-            {/if}
         {/each}
+        <hr class="w-12 my-2" style="border-color: var(--warm-grey); border-width: 1px 0 0 0;" />
     </div>
     <button class="mb-1">
         <img src="/img/icons/view-app-grid-symbolic.svg" alt="show-app" class="icon-show-app" />
@@ -88,7 +165,8 @@
     .app-icon {
         width: 50px;
         padding: 0.4rem;
-        margin: 0.08rem 0.3rem;
+        margin: 0.05rem 0.3rem;
+        -webkit-user-drag: none;
     }
 
     .app-icon:hover {
